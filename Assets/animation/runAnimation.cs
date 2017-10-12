76137ee9ab82c5d13388e1692535351d1afe104d
sehgalv <sehgalv@vcu.edu>
2017-10-12 01:56:56 -0400

    animation changes

--- Assets/animation/runAnimation.cs
+++ Assets/animation/runAnimation.cs
@@ -14,18 +14,18 @@ public class runAnimation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-       // var n = target.transform.position - body.transform.position;
+       //var n = target.transform.position - body.transform.position;
         print("picked up ");
-        if (Time.time >= 60.0f)//Random.Range(30.0f, 60.0f)
+        if (Time.time >= 30.0f)//Random.Range(30.0f, 60.0f)
         {
             anim.Play("idle");
-            if (Time.time >= 63.0f)
+            if (Time.time >= 33.0f)
             {
                 //body.transform.rotation = Quaternion.Euler(-Camera.main.transform.rotation.x, -Camera.main.transform.rotation.y, -Camera.main.transform.rotation.z);
                 print("body rotated");
-                //body.transform.rotation = Quaternion.LookRotation(-Camera.main.transform.up,-Camera.main.transform.forward);
-                body.transform.LookAt(target.transform, transform.up);
-               // body.transform.rotation = Quaternion.LookRotation(n) * Quaternion.Euler(0, 90, 0);
+                // body.transform.rotation = Quaternion.LookRotation(target.transform.up,-target.transform.forward);
+                 body.transform.LookAt(target.transform, target.transform.up);
+              // body.transform.rotation = Quaternion.LookRotation(n) * Quaternion.Euler(0, 90, 0);
             }
 
         } else
