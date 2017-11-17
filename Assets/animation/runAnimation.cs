ab75d200c187d660e008a3e596917d0619e3ce3c
zhangy53 <32204997+zhangy53@users.noreply.github.com>
2017-11-18 01:35:52 +0800

    animation additions

--- Assets/animation/runAnimation.cs
+++ Assets/animation/runAnimation.cs
@@ -23,17 +23,17 @@ public class runAnimation : MonoBehaviour {
         Quaternion targetMeasurement = Quaternion.Euler(0, (target.transform.position.x) *30.0f, 0);
         print("x position of camera"+target.transform.position.x);
         print("picked up ");
-        if (Time.time >= 60.0f)//Random.Range(30.0f, 60.0f)
+        if (Time.time >= 60.0f && Time.time < 75.0f)//Random.Range(30.0f, 60.0f)
         {
             anim.Play("idle");
             audio.Stop();
             main_audio.Stop();
             convo2.Stop();
             convo3.Stop();
-            if (Time.time >= 63.0f && (rotation ==false))
+            if (Time.time >= 63.0f && (rotation == false))
             {
-               
-                
+
+
 
                 //
 
@@ -42,10 +42,15 @@ public class runAnimation : MonoBehaviour {
                 {
                     rotation = true;
                     body.transform.Rotate(new Vector3(0, body.transform.rotation.y, 0));
-                    anim.Play("clapping");
-                } /*else
+
+
+
+                }
+
+                /*else
                 {
-                    body.transform.LookAt(target.transform.position);
+                    body.transform.LookAt(target.tr
+                    ansform.position);
                 }*/
                 // body.transform.rotation = Quaternion.LookRotation(target.transform.up,-target.transform.forward);
                 // body.transform.LookAt(target.transform); //, target.transform.up
@@ -53,11 +58,20 @@ public class runAnimation : MonoBehaviour {
                 // body.transform.rotation = Quaternion.LookRotation(n) * Quaternion.Euler(0, 90, 0);
 
 
-               
-               body.transform.rotation = Quaternion.Slerp(transform.rotation, targetMeasurement, Time.deltaTime * 2.0f);
+
+                body.transform.rotation = Quaternion.Slerp(transform.rotation, targetMeasurement, Time.deltaTime * 2.0f);
                 // print(body.GetInstanceID() + " "+body.position.x + " "+body.position.y +" " + body.position.z);
             }
 
+
+        }
+        else if (Time.time >= 75.0f && Time.time <90.0f)
+        {
+            anim.Play("clapping");
+        }
+
+        else if (Time.time >= 90.0f) {
+            anim.Play("sitting");
         } else
         {
             anim.Play("handmotion");
